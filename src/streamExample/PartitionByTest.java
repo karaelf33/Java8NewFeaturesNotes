4fe1c43a26da1bcb3d1ba3dcd937732d78793ffd
karaelf33 <bestasyusuf8@gmail.com>
2021-04-01 08:40:57 -0700

    grouping by stream  partionsBy examples

--- src/streamExample/PartitionByTest.java
+++ src/streamExample/PartitionByTest.java
@@ -2,16 +2,16 @@ package streamExample;
 
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class GroupingByTest {
+public class PartitionByTest {
 
     public static void main(String[] args) {
         Stream<String> s1=Stream.of("lions","tigers","bears","bird","horse","turtle","cat","cat","dog");
-        Map<Integer, List<String>> map=s1.collect(Collectors.groupingBy(String::length));
-
+        Predicate<String> p1=s->s.length()<5;
+        Map<Boolean, List<String>> map=s1.collect(Collectors.partitioningBy(p1));
         System.out.println(map);
 
     }
