c4074a60610bf38713754de2a8348a58148bac6f
karaelf33 <bestasyusuf8@gmail.com>
2021-04-05 23:32:23 -0700

    date api Instant

---
+++ src/dateApi/InstantTest.java
@@ -0,0 +1,12 @@
+package dateApi;
+
+import javafx.concurrent.Task;
+
+import java.time.Instant;
+
+public class InstantTest {
+    public static void main(String[] args) {
+        Instant instant=Instant.now();
+        System.out.println(instant);
+    }
+}
